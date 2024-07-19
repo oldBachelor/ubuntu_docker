04dc9b3ae96f721dc0dc0f70e45dfc50f8db2ecf
痞子 <49890150+oldBachelor@users.noreply.github.com>
2024-07-19 15:46:15 +0800

    Update ubuntu_docker.sh

--- ubuntu_docker.sh
+++ ubuntu_docker.sh
@@ -1,16 +1,25 @@
+### 安装 Docker 引擎脚本
+
+```bash
 #!/bin/bash
+
 # Remove existing packages
 for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do
   sudo apt-get remove $pkg
 done
+
 # Update package lists
 sudo apt-get update
+
 # Install necessary packages
 sudo apt-get install ca-certificates curl
+
 # Configure Docker repository
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://mirrors.ustc.edu.cn/docker-ce/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
+
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://mirrors.ustc.edu.cn/docker-ce/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
 # Install Docker packages
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
