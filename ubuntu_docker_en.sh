e30b410f33b54a6f86a19cf0ab764c1b5eb77e89
痞子 <49890150+oldBachelor@users.noreply.github.com>
2024-07-23 16:14:05 +0800

    Update ubuntu_docker_en.sh

--- ubuntu_docker_en.sh
+++ ubuntu_docker_en.sh
@@ -1,22 +1,18 @@
 #!/bin/bash
 
-# Remove existing packages
-for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do
-  sudo apt-get remove $pkg
-done
-
-# Update package lists
+# Add Docker's official GPG key:
 sudo apt-get update
-
-# Install necessary packages
 sudo apt-get install ca-certificates curl
-
-# Configure Docker repository
 sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://mirrors.ustc.edu.cn/docker-ce/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
 
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://mirrors.ustc.edu.cn/docker-ce/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
 
 # Install Docker packages
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
